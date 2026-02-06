c499909af1aa554d588eb4be5ba6f044f1ba7c7d
-g <raza.mushahid@rwit.io>
2026-02-06 13:36:33 +0530

    Developed a navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
+import Navbar from "@/components/sections/navbar/Navbar";
 import styles from "./page.module.scss";
+import DashboardLayout from "@/components/dashboardLayout/DashboardLayout";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        MultiStep Form
+        <Navbar />
       </main>
     </div>
   );
